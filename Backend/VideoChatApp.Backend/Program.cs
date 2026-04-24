4f00373608555fa2d0c419bf62046fb013ce87dd
ProfessorGA <alensaji82@gmail.com>
2026-04-24 12:09:45 +0530

    Fix: Final .NET 8.0 compatibility and privacy sync

--- Backend/VideoChatApp.Backend/Program.cs
+++ Backend/VideoChatApp.Backend/Program.cs
@@ -4,7 +4,6 @@ using VideoChatApp.Backend.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddOpenApi();
 
 builder.Services.AddSignalR();
 builder.Services.AddSingleton<IRoomService, RoomService>();
@@ -22,11 +21,6 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
 app.UseHttpsRedirection();
 
 app.UseCors("AllowAll");
